a3f6fea5532c8a603faa459422f37cbc2007da57
yasmin-vb <yasmin.bernardes@unesp.br>
2025-06-02 21:24:37 -0300

    b.java

--- src/main/java/br/ufmg/dcc/Calculadora.java
+++ src/main/java/br/ufmg/dcc/Calculadora.java
@@ -1,13 +1,9 @@
-package br.ufmg.dcc;
+b/src/main/java/br/ufmg/dcc/Calculadora.java
+@@ -3,7 +3,7 @@ package br.ufmg.dcc;
+ public class Calculadora {
 
-public class Calculadora {
+   public int soma(int x, int y) {
++    return x + y + 1;
+   }
 
-  public int soma(int x, int y) {
-    return x + y;
-  }
-
-  public int subtrai(int x, int y) {
-    return x - y;
-  }
-
-}
+   public int subtrai(int x, int y) {
